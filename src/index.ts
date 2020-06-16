02b8d0eef810728130db669a3177eb600c112c7f
adilpratama71 <adilpratama71@yahoo.com>
2020-06-16 13:54:23 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import fs from 'fs'
+import readline from 'readline'
+
+function checkRestriction(filepath: string): Promise<string | null> {
+  const pathName = __dirname + '/' + filepath
+  const rules = new RegExp(
+    `(.reduce\\()|(.map\\()|
+    (.filter\\()|(.indexOf\\()|(.toLocaleString\\()|
+    (.lastIndexOf\\()|(.reverse\\()|(.reduceRight\\()|(.includes\\()|
+    (.flat\\()|(.flatMap\\()|(.find\\()|(.findIndex\\()|(.fill\\()|
+    (.every\\()|(.copyWithin\\()|(.entries\\()|(.of\\()|(new Set\\()`,
+    'g'
+  )
+
+  const rl = readline.createInterface({
+    input: fs.createReadStream(pathName)
+  })
+
+  return new Promise(resolve => {
+    let lineNumber = 1
+    let result: null | string = null
+    rl.on('line', input => {
+      if (rules.test(input)) {
+        result = `${input.trim()}: ${lineNumber}`
+        rl.close()
+        rl.removeAllListeners()
+      }
+      lineNumber += 1
+    }).on('close', () => resolve(result))
+  })
+}
+
+export = checkRestriction
