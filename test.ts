e47efc70ece47f11568a1b251d0f620d5dcd319f
adilpratama71 <adilpratama71@yahoo.com>
2020-06-19 16:30:22 +0700

    delete test.ts

--- test.ts
+++
@@ -1,8 +0,0 @@
-import Restriction from './src/index'
-
-const checkRestriction = new Restriction('src/index.ts')
-console.log(checkRestriction.rules)
-checkRestriction.rules = ['concat', 'splice', 'opop']
-console.log(checkRestriction.rules)
-
-checkRestriction.readCode().then(result => console.log(result))
